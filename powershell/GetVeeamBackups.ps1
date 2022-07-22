e406b375a29849cb83d5b5c3623275630d8dadfe
tipoitkz <99466939+tipoitkz@users.noreply.github.com>
2022-07-22 22:37:13 +0600

    Update GetVeeamBackups.ps1

--- powershell/GetVeeamBackups.ps1
+++ powershell/GetVeeamBackups.ps1
@@ -7,10 +7,10 @@ function GetVeeamBackups {
 	foreach($u in $Backups) {
 	   $BackupsStr = "Type = Backups; Name ='" + $u.Name + "'; CreationTime = '" + $u.CreationTime + "'; LastPointCreationTime = " + $u.LastPointCreationTime + "; VmCount = " + $u.VmCount + "; JobType = " + $u.JobType + "; BackupPlatform = " + $u.BackupPlatform + "; TypeToString = " + $u.TypeToString + "; DirPath = '" + $u.DirPath + "'; PartialPath = '" + $u.PartialPath + "'; EncryptionState = " + $u.EncryptionState +"; MetaFileName = '" + $u.MetaFileName + "'; IsBackup = " + $u.IsBackup + ";"
 	   
-       $BackupsStr
+       	   $BackupsStr
        
-       #Uncomment if you want to store in file
-	     #$BackupsStr | Out-File -FilePath E:\execbeat-3.3.0-windows-x86_64\VBRBackup.txt -Append
+           #Uncomment if you want to store in file
+           #$BackupsStr | Out-File -FilePath E:\execbeat-3.3.0-windows-x86_64\VBRBackup.txt -Append
 	}
 	
 }
